0b5a3dc82cb0dd0f3a23069069bd2e6fb3934259
claymays <claymays854@gmail.com>
2021-09-29 19:26:58 -0500

    Initialization

---
+++ src/main/java/com/mays/mtgboostergame/MtgBoosterGameApplication.java
@@ -0,0 +1,13 @@
+package com.mays.mtgboostergame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtgBoosterGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtgBoosterGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mays/mtgboostergame/MtgBoosterGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mays.mtgboostergame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtgBoosterGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
